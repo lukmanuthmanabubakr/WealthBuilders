357991b4cd76056c87a17cd58f998b0cd2383ffe
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-12-23 11:24:31 +0100

    updated

--- src/Payment/Dashboard/Dashboard.js
+++ src/Payment/Dashboard/Dashboard.js
@@ -1,20 +1,33 @@
-import "./Dashboard.css";
-import Notification from '../../components/Notification/Notification';
-import DashLink from "../DashLink/DashLink";
-import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
-import { useSelector } from "react-redux";
+import React from 'react'
 
 const Dashboard = () => {
-  const { user } = useSelector((state) => state.auth);
-  const isVerified = user?.isVerified;
-
-  useRedirectLoggedOutUser("/login");
   return (
-    <div className="dashContainer">
-      {isVerified === false && <Notification />}
-      <DashLink />
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
+
+
+// import "./Dashboard.css";
+// import Notification from '../../components/Notification/Notification';
+// import DashLink from "../DashLink/DashLink";
+// import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
+// import { useSelector } from "react-redux";
+
+// const Dashboard = () => {
+//   const { user } = useSelector((state) => state.auth);
+//   const isVerified = user?.isVerified;
+
+//   useRedirectLoggedOutUser("/login");
+//   return (
+//     <div className="dashContainer">
+//       {isVerified === false && <Notification />}
+//       <DashLink />
+//     </div>
+//   );
+// };
+
+// export default Dashboard;

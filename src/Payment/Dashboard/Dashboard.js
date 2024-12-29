e082912457d73d321474f59c1e995b2cf6079799
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-12-29 10:34:24 +0100

    good

--- src/Payment/Dashboard/Dashboard.js
+++ src/Payment/Dashboard/Dashboard.js
@@ -1,14 +1,26 @@
 import "./Dashboard.css";
-import Notification from '../../components/Notification/Notification';
+import Notification from "../../components/Notification/Notification";
 import DashLink from "../DashLink/DashLink";
 import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Dashboard = () => {
   const { user } = useSelector((state) => state.auth);
   const isVerified = user?.isVerified;
 
   useRedirectLoggedOutUser("/login");
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = process.env.REACT_APP_JIVOCHAT_URL;
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div className="dashContainer">
       {isVerified === false && <Notification />}

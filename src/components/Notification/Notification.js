a535df142d2897fd987581b4960d008681b791c8
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-12-31 15:33:06 +0100

    good

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,7 +1,9 @@
-
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { RESET, sendVerificationEmail } from "../../redux/features/auth/authSlice";
+import {
+  RESET,
+  sendVerificationEmail,
+} from "../../redux/features/auth/authSlice";
 import { FiAlertCircle } from "react-icons/fi"; // Alert icon
 import "./Notification.css";
 import ButtonLoader from "../ButtonLoader/ButtonLoader";
@@ -15,19 +17,37 @@ const Notification = () => {
     await dispatch(RESET());
   };
 
-  const { isLoading } =
-  useSelector((state) => state.auth);
+  const { isLoading } = useSelector((state) => state.auth);
 
   return (
     <div className="notification-container">
       <div className="notification-card">
         <FiAlertCircle className="alert-icon" />
         <div className="notification-content">
-          <p><b>Account Verification Needed</b></p>
-          <p>Check your email for a verification link to activate your account, If you did not receive email</p>
-          <NavLink isLoading={isLoading} className="resend-button" target="_blank" to="https://www.wealtybuilders.com/contact">
-            Contact Support
-          </NavLink>
+          <p>
+            <b>Account Verification Needed</b>
+          </p>
+          <p>
+            Check your email for a verification link to activate your account,
+            If you did not receive email
+          </p>
+          <div className="navBtsn">
+            <ButtonLoader
+              isLoading={isLoading}
+              className="resend-button"
+              onClick={sendVerEmail}
+            >
+              Resend Verification Link
+            </ButtonLoader>
+            <NavLink
+              isLoading={isLoading}
+              className="contact-button"
+              target="_blank"
+              to="https://www.wealtybuilders.com/contact"
+            >
+              Contact Support
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>

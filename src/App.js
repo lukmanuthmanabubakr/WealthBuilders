21c2eec87b5f8c04cac0423207c036cd0f4fa064
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-01-20 00:31:05 +0100

    update

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ const App = () => {
           <Route path="/user-profile" element={<Profile />} />
         </Routes>
       </GoogleOAuthProvider>
-      {/* <Loader /> */}
+      {/* <L /> */}
     </div>
   );
 };
